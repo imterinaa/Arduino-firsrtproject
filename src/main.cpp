41975e34f29aba8df76afc15edf14ba700942d34
imterinaa <84046726+imterinaa@users.noreply.github.com>
2023-10-29 23:07:17 +0300

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,176 @@
+#if !( defined(ESP32) )
+  #error This code is designed for (ESP32 + ENC28J60) to run on ESP32 platform! Please check your Tools->Board setting.
+#endif
 
-// #define EB_NO_FOR           // отключить поддержку pressFor/holdFor/stepFor и счётчик степов (экономит 2 байта оперативки)
-// #define EB_NO_CALLBACK      // отключить обработчик событий attach (экономит 2 байта оперативки)
-// #define EB_NO_COUNTER       // отключить счётчик энкодера (экономит 4 байта оперативки)
-// #define EB_NO_BUFFER        // отключить буферизацию энкодера (экономит 1 байт оперативки)
+#define DEBUG_ETHERNET_WEBSERVER_PORT       Serial
 
-// #define EB_DEB_TIME 50      // таймаут гашения дребезга кнопки (кнопка)
-// #define EB_CLICK_TIME 500   // таймаут ожидания кликов (кнопка)
-// #define EB_HOLD_TIME 600    // таймаут удержания (кнопка)
-// #define EB_STEP_TIME 200    // таймаут импульсного удержания (кнопка)
-// #define EB_FAST_TIME 30     // таймаут быстрого поворота (энкодер)
+// Debug Level from 0 to 4
+#define _ETHERNET_WEBSERVER_LOGLEVEL_       3
 
-// SDA = D21; SCL = D22;
+//////////////////////////////////////////////////////////
 
-// #include <EncButton.h>
-// EncButton eb(27, 26, 25); // S1 = 27; S2 = 26; Key = 25;
-// Adafruit_AHTX0 aht;
-// EncButton eb(2, 3, 4, INPUT); // + режим пинов энкодера
-// EncButton eb(2, 3, 4, INPUT, INPUT_PULLUP); // + режим пинов кнопки
+// Optional values to override default settings
+//#define ETH_SPI_HOST        SPI3_HOST
+//#define SPI_CLOCK_MHZ       20
 
+// Must connect INT to GPIOxx or not working
+//#define INT_GPIO            4
 
-#include <Arduino.h>
-#include "Wrapper.h"
+//#define MISO_GPIO           19
+//#define MOSI_GPIO           23
+//#define SCK_GPIO            18
+//#define CS_GPIO             5
 
-wrapperAHT aht;
-wrapperEncoder enc;
+//////////////////////////////////////////////////////////
 
-void setup() {
+#include <WebServer_ESP32_ENC.h>
 
-	Serial.begin(115200);
-	aht.setup();
-	enc.setup();
+WiFiServer server(80);
+
+// Enter a MAC address and IP address for your controller below.
+#define NUMBER_OF_MAC      20
+
+byte mac[][NUMBER_OF_MAC] =
+{
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x01 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x02 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x03 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x04 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x05 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x06 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x07 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x08 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x09 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x0A },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x0B },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x0C },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x0D },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x0E },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x0F },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x10 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x11 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x12 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x13 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x14 },
+};
+
+// Select the IP address according to your local network
+IPAddress myIP(192, 168, 2, 232);
+IPAddress myGW(192, 168, 2, 1);
+IPAddress mySN(255, 255, 255, 0);
+
+// Google DNS Server IP
+IPAddress myDNS(8, 8, 8, 8);
+
+int reqCount = 0;                // number of requests received
+
+void setup()
+{
+  Serial.begin(115200);
+
+  while (!Serial && (millis() < 5000));
+
+  Serial.print(F("\nStart WebServer on "));
+  Serial.print(ARDUINO_BOARD);
+  Serial.print(F(" with "));
+  Serial.println(SHIELD_TYPE);
+  Serial.println(WEBSERVER_ESP32_ENC_VERSION);
+
+  ET_LOGWARN(F("Default SPI pinout:"));
+  ET_LOGWARN1(F("MOSI:"), MOSI_GPIO);
+  ET_LOGWARN1(F("MISO:"), MISO_GPIO);
+  ET_LOGWARN1(F("SCK:"),  SCK_GPIO);
+  ET_LOGWARN1(F("CS:"),   CS_GPIO);
+  ET_LOGWARN1(F("INT:"),  INT_GPIO);
+  ET_LOGWARN1(F("SPI Clock (MHz):"), SPI_CLOCK_MHZ);
+  ET_LOGWARN(F("========================="));
+
+  ///////////////////////////////////
+
+  // To be called before ETH.begin()
+  ESP32_ENC_onEvent();
+
+  // start the ethernet connection and the server:
+  // Use DHCP dynamic IP and random mac
+  //bool begin(int MISO_GPIO, int MOSI_GPIO, int SCLK_GPIO, int CS_GPIO, int INT_GPIO, int SPI_CLOCK_MHZ,
+  //           int SPI_HOST, uint8_t *W6100_Mac = W6100_Default_Mac);
+  ETH.begin( MISO_GPIO, MOSI_GPIO, SCK_GPIO, CS_GPIO, INT_GPIO, SPI_CLOCK_MHZ, ETH_SPI_HOST );
+  //ETH.begin( MISO_GPIO, MOSI_GPIO, SCK_GPIO, CS_GPIO, INT_GPIO, SPI_CLOCK_MHZ, ETH_SPI_HOST, mac[millis() % NUMBER_OF_MAC] );
+
+  // Static IP, leave without this line to get IP via DHCP
+  //bool config(IPAddress local_ip, IPAddress gateway, IPAddress subnet, IPAddress dns1 = 0, IPAddress dns2 = 0);
+  ETH.config(myIP, myGW, mySN, myDNS);
+
+  ESP32_ENC_waitForConnect();
+
+  ///////////////////////////////////
+
+  // start the web server on port 80
+  server.begin();
 }
 
-void loop() {
-	enc.print();
-	enc.event_click(aht);
+void loop()
+{
+  // listen for incoming clients
+  WiFiClient client = server.available();
+
+  if (client)
+  {
+    Serial.println(F("New client"));
+    // an http request ends with a blank line
+    bool currentLineIsBlank = true;
+
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        char c = client.read();
+        Serial.write(c);
+
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank)
+        {
+          Serial.println(F("Sending response"));
+
+          // send a standard http response header
+          // use \r\n instead of many println statements to speedup data send
+          client.print(
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"  // the connection will be closed after completion of the response
+            "Refresh: 20\r\n"        // refresh the page automatically every 20 sec
+            "\r\n");
+          client.print("<!DOCTYPE HTML>\r\n");
+          client.print("<html>\r\n");
+          client.print(String("<h2>Hello World from ") + BOARD_NAME + "!</h2>\r\n");
+          client.print("Requests received: ");
+          client.print(++reqCount);
+          client.print("<br>\r\n");
+          client.print("<br>\r\n");
+          client.print("</html>\r\n");
+          break;
+        }
+
+        if (c == '\n')
+        {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r')
+        {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+
+    // give the web browser time to receive the data
+    delay(10);
+
+    // close the connection:
+    client.stop();
+    Serial.println(F("Client disconnected"));
+  }
 }
